57b2b60940fae934ef8d8e9dc977004e486ce811
odow <o.dowson@gmail.com>
2020-06-26 12:26:36 -0500

    Move to binaries hosted at wisc.edu

--- deps/build.jl
+++ deps/build.jl
@@ -1,29 +1,21 @@
 import Libdl
 
+const DEFAULT_PATH_URL = "http://pages.cs.wisc.edu/~ferris/path/julia/"
+
 function download_path()
-    if !haskey(ENV, "SECRET_URL_PATH_BINARIES")
-        error(
-            "Unable to download PATH. Set the environment variable " *
-            "`SECRET_URL_PATH_BINARIES`, or install a copy of `libpath50.xx` manually " *
-            "and set the environment variable `PATH_JL_LOCATION` to point to the libpath " *
-            "location."
-        )
-    end
-    base_url = ENV["SECRET_URL_PATH_BINARIES"]
-    libpath, libgfortran = if Sys.islinux() &&  Sys.ARCH == :x86_64
-        "libpath50.so", "libgfortran.so.4.0.0"
+    libpath = if Sys.islinux() &&  Sys.ARCH == :x86_64
+        "libpath50.so"
     elseif Sys.isapple()
-        "libpath50.dylib", "libgfortran.4.dylib"
+        "libpath50.dylib"
     elseif Sys.iswindows()
-        "libpath50.dll", "libgfortran-4.dll"
+        "path50.dll"
     else
         error(
             "Unsupported operating system. Only 64-bit linux, OSX, and Windows are supported."
         )
     end
     ENV["PATH_JL_LOCATION"] = joinpath(@__DIR__, libpath)
-    download(joinpath(base_url, libpath), ENV["PATH_JL_LOCATION"])
-    download(joinpath(base_url, libgfortran), joinpath(@__DIR__, libgfortran))
+    download(joinpath(DEFAULT_PATH_URL, libpath), ENV["PATH_JL_LOCATION"])
     return
 end
 
@@ -34,7 +26,7 @@ function install_path()
     local_filename = get(ENV, "PATH_JL_LOCATION", nothing)
     if local_filename === nothing
         error("Environment variable `PATH_JL_LOCATION` not found.")
-    elseif Libdl.dlopen_e(local_filename) == C_NULL
+    elseif Libdl.dlopen(local_filename) == C_NULL
         error("Unable to open the path library $(local_filename).")
     end
     open("deps.jl", "w") do io
